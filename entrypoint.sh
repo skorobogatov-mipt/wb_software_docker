c3713d9b6c4343e1357bd5406410b9595898e96e
spanovs01 <panov.ss@phystech.edu>
2024-06-20 23:53:59 +0300

    dirty hack. specify launch file. It will work only with one taskgit add entrypoint.sh !

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@ wget $WORKSPACE_DOWNLOAD_LINK -O robot_software.zip
 unzip -q robot_software.zip -d /root/workspace
 rm -r robot_software.zip
 source /root/workspace/install/setup.bash
-ros2 launch controller controller.launch
+ros2 launch controller motion_task.launch
